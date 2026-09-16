a7985ed500ca866a6742244220a6c52adee6d105
Walrider0 <toth.bence.szabolcs@gmail.com>
2026-09-16 19:59:31 +0200

    Sidebar animation, bug fixes

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-function toggleSubMenu(button) {
+const toggleButton = document.getElementById('toggle-btn')
+const sidebar = document.getElementById('sidebar')
+
+function toggleSidebar() 
+{
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
+}
+
+
+function toggleSubMenu(button) 
+{
     button.nextElementSibling.classList.toggle('show')
     button.classList.toggle('rotate')
 }
