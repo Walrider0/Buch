fca74adeb0ee17511ace04986e44fede7eea1a72
Walrider0 <toth.bence.szabolcs@gmail.com>
2026-09-22 21:30:46 +0200

    fixes, Responsive
    Made some fixes, new tabs and changed it for mobile visitors.

--- app.js
+++ app.js
@@ -7,29 +7,37 @@ function toggleSidebar()
     sidebar.classList.toggle('close')
     toggleButton.classList.toggle('rotate')
 
-    Array.from(sidebar.getElementsByClassName('show'))
-    .forEach(ul => {
-        ul.classList.remove('show')
-        ul.previousElementSibling.classList('rotate')
-    }
-    )       //To close all the sub menus while the sidebar is closed
-
+    closeAllSubMenus()
 }
 
 
 
 function toggleSubMenu(button) //This handles the sub menu
 {
+
+    if(!button.nextElementSibling.classList.contains('show'))
+    {
+        closeAllSubMenus()
+    }
+    
+
     button.nextElementSibling.classList.toggle('show')
     button.classList.toggle('rotate')
 
     if(sidebar.classList.contains('close'))
     {
-        //sidebar.classList.toggle('close')
-        //toggleButton.classList.toggle('rotate')
-        toggleSidebar()
+        sidebar.classList.toggle('close')
+        toggleButton.classList.toggle('rotate')
+        
     }
-    
+}
 
+function closeAllSubMenus()
+{
+    Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+        ul.classList.remove('show')
+        ul.previousElementSibling.classList.remove('rotate')
+    }
+    )       //To close all the sub menus while the sidebar is closed
 
 }
